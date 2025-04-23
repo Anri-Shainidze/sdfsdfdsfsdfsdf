7d992a1793f84d48874dff7d7f5794bc016d3082
Anri-Shainidze <anrianu2006@gmail.com>
2025-04-23 21:14:58 +0400

    kk

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+
+const ScrollAnimation = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
+  });
+
+  // scrollYProgress: 0 → ელემენტი ბოლომდე ჯერ არ გამოჩენილა
+  // scrollYProgress: 1 → მთლიანად გამოჩნდა viewport-ში
+
+  const y = useTransform(scrollYProgress, [0, 1], [100, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [0, 1]);
+  const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const rotate = useTransform(scrollYProgress, [0, 1], ["-10deg", "0deg"]);
+
+  return (
+    <div style={{ height: "200vh", paddingTop: "100vh" }}>
+      <motion.div
+        ref={ref}
+        style={{
+          y,
+          opacity,
+          scale,
+          rotate,
+          background: "tomato",
+          height: 200,
+          width: 300,
+          margin: "0 auto",
+          borderRadius: 20,
+        }}
+      >
+        Scroll Animate!
+      </motion.div>
+    </div>
+  );
+};
+
+export default ScrollAnimation;
+
